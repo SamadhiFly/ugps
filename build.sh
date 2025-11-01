bf5fc079253bf41bac35663a373cf4c5e1df7eb4
SamadhiFly <lsgx@live.com>
2025-11-01 21:07:56 +0800

    Add build.sh for onekey building
    Signed-off-by: SamadhiFly <lsgx@live.com>

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#filename: build.sh
+
+BUILD_NINJA_FILE=build.ninja
+CMAKE_PRESETS_FILE=CMakePresets.json
+CMAKE_PRESETS_CONFIG=config-openwrt
+CMAKE_PRESETS_BUILD=build-openwrt
+
+[ -e ${BUILD_NINJA_FILE} ] && echo "### cleaning..." && cmake --build --preset ${CMAKE_PRESETS_BUILD} --target clean
+
+files=("build.ninja" "install_manifest.txt" "compile_commands.json" "CMakeCache.txt" "cmake_install.cmake")
+for file in "${files[@]}"; do [ -f "$file" ] && echo "removing file ${file}" && rm -f "$file"; done
+
+dirs=("CMakeFiles")
+for dir in "${dirs[@]}"; do [ -d "$dir" ] && echo "removing dir ${dir}" && rm -rf "$dir"; done
+
+[ -e ${CMAKE_PRESETS_FILE} ] && echo "### configuring..." && cmake --preset ${CMAKE_PRESETS_CONFIG}
+
+[ -e ${BUILD_NINJA_FILE} ] && echo "### building..." && cmake --build --preset ${CMAKE_PRESETS_BUILD} --target all
+
