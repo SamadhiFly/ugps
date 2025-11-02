6ff1c995731ccf5a0e774ba4adbdbf375003fbab
SamadhiFly <lsgx@live.com>
2025-11-02 12:18:12 +0800

    Change CMAKE_EXPORT_COMPILE_COMMANDS Option for CMakePresets.json
    Signed-off-by: SamadhiFly <lsgx@live.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 #filename: build.sh
 
-BUILD_NINJA_FILE=build.ninja
-CMAKE_PRESETS_FILE=CMakePresets.json
-CMAKE_PRESETS_CONFIG=config-openwrt
-CMAKE_PRESETS_BUILD=build-openwrt
+TARGET_FILE_NAME='ugps'
+TARGET_FILE_EXT='.elf'
+
+BUILD_NINJA_FILE='build.ninja'
+CMAKE_PRESETS_FILE='CMakePresets.json'
+CMAKE_PRESETS_CONFIG='config-openwrt'
+CMAKE_PRESETS_BUILD='build-openwrt'
+
+[ -e ${TARGET_FILE_NAME}${TARGET_FILE_EXT} ] && echo "### deleting target ${TARGET_FILE_NAME}${TARGET_FILE_EXT}" && rm -f ${TARGET_FILE_NAME}${TARGET_FILE_EXT}
 
 [ -e ${BUILD_NINJA_FILE} ] && echo "### cleaning..." && cmake --build --preset ${CMAKE_PRESETS_BUILD} --target clean
 
@@ -18,3 +23,7 @@ for dir in "${dirs[@]}"; do [ -d "$dir" ] && echo "removing dir ${dir}" && rm -r
 
 [ -e ${BUILD_NINJA_FILE} ] && echo "### building..." && cmake --build --preset ${CMAKE_PRESETS_BUILD} --target all
 
+[ -e ${TARGET_FILE_NAME} ] && echo "### removing target ${TARGET_FILE_NAME}" && mv -f ${TARGET_FILE_NAME} ${TARGET_FILE_NAME}${TARGET_FILE_EXT}
+
+echo "### build.sh done."
+exit 0
--- clean.sh
+++ clean.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-#filename: build.sh
+#filename: clean.sh
 
-BUILD_NINJA_FILE=build.ninja
-CMAKE_PRESETS_FILE=CMakePresets.json
-CMAKE_PRESETS_CONFIG=config-openwrt
-CMAKE_PRESETS_BUILD=build-openwrt
+TARGET_FILE_NAME='ugps'
+TARGET_FILE_EXT='.elf'
+
+BUILD_NINJA_FILE='build.ninja'
+CMAKE_PRESETS_FILE='CMakePresets.json'
+CMAKE_PRESETS_CONFIG='config-openwrt'
+CMAKE_PRESETS_BUILD='build-openwrt'
+
+[ -e ${TARGET_FILE_NAME}${TARGET_FILE_EXT} ] && echo "### deleting target ${TARGET_FILE_NAME}${TARGET_FILE_EXT}" && rm -f ${TARGET_FILE_NAME}${TARGET_FILE_EXT}
 
 [ -e ${BUILD_NINJA_FILE} ] && echo "### cleaning..." && cmake --build --preset ${CMAKE_PRESETS_BUILD} --target clean
 
@@ -14,3 +19,5 @@ for file in "${files[@]}"; do [ -f "$file" ] && echo "removing file ${file}" && 
 dirs=("CMakeFiles")
 for dir in "${dirs[@]}"; do [ -d "$dir" ] && echo "removing dir ${dir}" && rm -rf "$dir"; done
 
+echo "### clean.sh done."
+exit 0
